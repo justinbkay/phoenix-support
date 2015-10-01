4c5ab6b2801b5776821187c6e3601ceb21847c41
Justin <justinbkay@gmail.com>
2015-10-01 13:17:02 -0600

    add a user and display the name on the default page

--- web/controllers/registration_controller.ex
+++ web/controllers/registration_controller.ex
@@ -3,12 +3,26 @@ defmodule Support.RegistrationController do
 
   alias Support.User
 
+  plug :scrub_params, "user" when action in [:create]
+
   def new(conn, _params) do
     changeset = User.changeset(%User{})
     render conn, "new.html", changeset: changeset
   end
 
-  def create(conn, params) do
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "reg success!")
+        |> put_session(:current_user, user.id)
+        |> redirect(to: "/")
+      {:error, changeset} ->
+        conn
+        |> render("new.html", changeset: changeset)
+    end
   end
 
 end
--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Support.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Support.CurrentUser
   end
 
   pipeline :api do
