81735f6f85da124a3c0a2eb1c5e913e032ab5ba8
Justin <justinbkay@gmail.com>
2015-10-01 13:58:16 -0600

    ability to log into the app

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,6 +1,29 @@
 defmodule Support.SessionController do
   use Support.Web, :controller
 
+  import Comeonin.Bcrypt, only: [checkpw: 2]
+
+  alias Support.User
+
+  def new(conn, _params) do
+    render conn, "new.html"
+  end
+
+  def create(conn, %{"session" => %{"email" => email, "password" => password}}) do
+    user = Repo.get_by(User, email: email)
+    if user && checkpw(password, user.encrypted_password) do
+      conn
+      |> put_session(:current_user, user.id)
+      |> put_flash(:info, "Logged in")
+      |> redirect(to: page_path(conn, :index))
+    else
+      conn
+      |> put_flash(:error, "Bad things just happened")
+      |> render("new.html")
+    end
+
+  end
+
   def delete(conn, _params) do
     conn
     |> delete_session(:current_user)
